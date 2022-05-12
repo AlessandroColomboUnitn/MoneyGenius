2cfe5d1c77a56f8918705228946d28833cc29893
acolombo <alessandro.colombo-1@studenti.unitn.it>
2022-05-12 16:11:06 +0200

    Add user registration.

--- app/authentication.js
+++ app/authentication.js
@@ -4,6 +4,7 @@ const User = require('./models/user'); // get our mongoose model
 const jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
 
 // ---------------------------------------------------------
+// Based on source: https://github.com/unitn-software-engineering/EasyLib/blob/master/app/authentications
 // route to authenticate and get a new token
 // ---------------------------------------------------------
 router.post('', async function(req, res) {
@@ -45,6 +46,51 @@ router.post('', async function(req, res) {
 
 });
 
+// ---------------------------------------------------------
+//  Route to register a new user and get a new token.
+// ---------------------------------------------------------
+router.put('', async function(req, res) {
+	
+	
+    if(checkIfEmailInString(req.body.email)){
+        // build a new user object
+	    let user = new User({
+            email: req.body.email,
+            password: req.body.password
+        });
 
+        user = await user.save(); //save user in DB
+
+        //create a token
+	    var payload = {
+		    email: user.email,
+		    id: user._id	
+	    }
+	    var options = {
+		    expiresIn: 86400 // expires in 24 hours
+	    }
+	    var token = jwt.sign(payload, process.env.SUPER_SECRET, options);
+	    res.json({
+		    success: true,
+		    message: 'Enjoy your token!',
+		    token: token,
+		    email: user.email,
+		    id: user._id,
+		    self: "api/v1/" + user._id
+	    });
+    }
+
+    else{
+		res.json({ success: false, message: 'Registration failed. You have to provide a valid e-mail.' })
+    }
+
+});
+
+// source: https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+function checkIfEmailInString(text) {
+    // eslint-disable-next-line
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(text);
+}
 
 module.exports = router;
