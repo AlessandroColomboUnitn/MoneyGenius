bd90fe930ee1d22372ff7dbf83a7d01ef32f20ea
acolombo <alessandro.colombo-1@studenti.unitn.it>
2022-05-17 11:49:48 +0200

    Add login API.

--- app/app.js
+++ app/app.js
@@ -1,7 +1,37 @@
 const express = require('express');
 const app = express();
 
+//For API documentation
+const swaggerUi = require('swagger-ui-express');
+const swaggerJSDoc = require('swagger-jsdoc');
+
+const swaggerDefinition = {
+    openapi: '3.0.0',
+    info: {
+        title: 'MoneyGenius API',
+        description: 'API for managing personal finance and sharing common expenses.',
+        version: '1.0.0',
+    },
+    servers:[
+        {
+            url: 'http://localhost:8080',
+            description: 'Development server',
+        }
+    ],
+};
+
+const options = {
+    swaggerDefinition,
+    apis: ['./routes/*.js'],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+//Login and signup
 const authentication = require("./authentication")
+//Cheks validity of JWT
 const tokenChecker = require("./tokenChecker")
 
 const users = require('./users.js')
@@ -9,6 +39,7 @@ const users = require('./users.js')
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
+//basic user interface 
 app.use('/', express.static('static'));
 
 /**
