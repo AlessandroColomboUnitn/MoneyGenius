b7e0d21bca78f7406d8445bd98d45cbf15a66d96
acolombo <alessandro.colombo-1@studenti.unitn.it>
2022-05-19 08:54:45 +0200

    Add response code after successfull login.

--- app/authentication.js
+++ app/authentication.js
@@ -32,7 +32,7 @@ router.post('/login', async function(req, res) {
 	}
 	var token = jwt.sign(payload, process.env.SUPER_SECRET, options);
 
-	res.json({
+	res.status(200).json({
 		success: true,
 		message: 'Enjoy your token!',
 		token: token,
