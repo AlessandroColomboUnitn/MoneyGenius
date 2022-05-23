c7259f6febb9b812dab5cd7d938087362da64a99
mattia <mattia.rampazzo@studenti.unitn.it>
2022-05-23 07:58:40 +0200

    Split UI expenses script functions from script.js into new script file expense.js and fix validateInput

--- static/script/script.js
+++ static/script/script.js
@@ -2,15 +2,15 @@
  * This variable stores the logged in user
  */
 var loggedUser = {};
+var base="http://localhost:8080";
+
 
 function assert(condition, message){
     if (!condition) throw message || "assertion failed";
 }
-var base="http://localhost:8080";
-
 
 //code for the modal, code taken from w3schools.com
-function loadModal(){
+function loadModals(){
     // Get the modals
     var mdlExpense = document.getElementById("mdlExpense");
     var mdlCategory = document.getElementById("mdlCategory");
@@ -45,34 +45,6 @@ function loadModal(){
     }
 }
 
-/*
-//checks the input of the expense form, called when the button is clicked
-function validateInputs(){
-    return validateAmount();
-    //checkDate() date should not be int the future or before 2000 for examples
-}
-
-//if the amount is not a number or is less than zero send an alert
-function validateAmount(){
-    //let v = document.forms["expenseForm"]["amount"].value;
-    let v = document.getElementById("amount").value;
-    
-    //if(isNaN(v) || v<=0){
-    if (v <= 0) {
-        alert("Errore: inserito Totale negativo");
-        return false;
-    }
-    
-    return true;
-}
-//even this properties can be obatained with the min name on the input tag
-*/
-
-function clearDate(date){
-    //split in two the string and return the first half
-    return date.split('T')[0];
-}
-
 
 //displays login form after login selection
 function displayLogin(){
@@ -105,13 +77,11 @@ function afterAuth(){
     //document.getElementById("loggedUser").innerHTML = loggedUser.name;
     document.getElementById("navAuthentication").hidden = true;
     document.getElementById("divAuthentication").hidden = true;
-    //document.getElementById("divExpense").hidden = false;
-    document.getElementById("divBudget").hidden = false;
+    
     document.getElementById("viewBudgetLabel").hidden = false;
     document.getElementById("budgetRimanente").hidden = false;
     
     //set user's default category
-    
     fetch('../api/v1/users/'+loggedUser.id+'/categories/default', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
@@ -125,13 +95,14 @@ function afterAuth(){
         window.alert(error);
     });
 
-
     //fetch the budget html
     fetch('./budget.html')
     .then(response=> response.text())
     .then(text => {
-        let divBudget = document.getElementById("divBudget");
-        divBudget.innerHTML = text;
+        document.getElementById("divBudget").innerHTML = text;
+
+        viewBudget();
+
     });
 
     //fetch the category html
@@ -146,24 +117,16 @@ function afterAuth(){
     fetch('./expense.html')
     .then(response=> response.text())
     .then(text => {
-        let divExpense = document.getElementById("divExpense");
-        divExpense.innerHTML = text;
+        document.getElementById("divExpense").innerHTML = text;
 
-    //load the expenses
-    loadExpensesList();
-    //load the expense modal
-    loadModal();
-    //load the category drop down list 
-    loadCategoriesOptions();
+        //load the expenses
+        loadExpensesList();
+        //load the expense modal
+        loadModals();
+        //load the category drop down list 
+        loadCategoriesOptions();
 
     });
-
-    
-
-
-    //loads the expenses
-    viewBudget();
-
 }
 
 function afterSetBudget(){
@@ -274,157 +237,6 @@ function setBudget(){
     })
 }
 
-//send an asynchronous request to the api to retrieve the list of epenses
-//if there arent any expenses do not show the table...
-function loadExpensesList(){
-
-    let url = new URL('api/v1/users/' + loggedUser.id + '/expenses', base);
-    let params = {token:loggedUser.token};
-    Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
-    
-    fetch(url)
-    .then((resp) => resp.json()) // Transform the data into json
-    .then(function(data){
-        if(!data.success){
-            console.log(data);
-            throw data.message;
-        }else{
-
-            let expensesList = document.getElementById("expensesList");
-            let userExpenses = data.expenses;
-
-            //if i have any expense
-            if(userExpenses.length>0){
-                let table = createExpensesTable();
-                table = fillExpensesTable(userExpenses, table);       
-                expensesList.appendChild(table);
-            }else{
-                let span = document.createElement("span");
-                span.innerHTML="Nessuna spesa registrata"; 
-                expensesList.appendChild(span);
-            }
-
-            return;
-        }
-    })
-    .catch( 
-        (error) => {
-            window.alert(error);
-            console.error(error);
-        }
-    ); // If there is any error you will catch them here
-}
-
-
-function loadCategoriesOptions(){
-    let url = new URL('api/v1/users/' + loggedUser.id + '/categories', base);
-    let params = {token:loggedUser.token};
-    Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
- 
-
-    fetch(url)
-    .then((resp) => resp.json()) // Transform the data into json
-    .then(function(data){
-        if(!data.success){
-            console.log(data);
-            throw data.message;
-        }else{
-
-            let categoriesSel = document.getElementById("categoryId");
-            let userCategories = data.categories;
-
-            userCategories.forEach(category => {
-                categoriesSel.options[categoriesSel.options.length] = new Option( category.name, category._id,);
-            })
-
-            return;
-        }
-    })
-    .catch( 
-        (error) => {
-            window.alert(error);
-            console.error(error);
-        }
-    );
-
-}
-
-function addExpense(){
-
-    let url = new URL('api/v1/users/' + loggedUser.id + '/expenses', base);
-
-    var name = document.getElementById("name").value;
-    var amount = document.getElementById("amount").value;
-    var categoryId = document.getElementById("categoryId").value;
-    var date = document.getElementById("date").value;
-
-    fetch(url, {
-        method: 'POST',
-        headers: {'Content-type': 'application/json'},
-        body: JSON.stringify({
-            token: loggedUser.token, 
-            name: name,
-            amount: amount,
-            categoryId: categoryId,
-            date: date
-        })
-    })
-    .then((resp) => resp.json())
-    .then(function(data){
-
-        if(data.success){
-            
-            window.alert("Nuova spesa registrata");
-                        
-            let table = document.getElementById('expensesTable');
-            let expense = data.expense;
-            //let budget = data.budget;
-            //let budget_spent = data.budget_spent;
-            
-            //create table if its the first expense
-            if(!table){
-                let expensesList = document.getElementById("expensesList");
-               
-                //get the span and remove it
-                let span = expensesList.firstElementChild;
-                expensesList.removeChild(span);
-
-                //create the table and append it
-                table = createExpensesTable();
-                table = fillExpensesTable(new Array(expense), table);
-                expensesList.appendChild(table);
-            }else{
-                //else just update it 
-                table = fillExpensesTable(new Array(expense), table);
-            }
-
-            //get close modal icon
-            let span = document.getElementById("spanCloseExpenseForm");
-
-            //close the modal
-            span.dispatchEvent(new MouseEvent('click'));
-
-            //reset form
-            document.getElementById('expenseForm').reset();
-
-            //update budget UI
-            viewBudget();
-            /*
-            //update budget and budget_spent
-            document.getElementById("budgetSpentView").innerHTML = budget_spent;
-            
-            if(!isNaN(budget))
-                document.getElementById("budget2View").innerHTML = budget;
-            */
-        }
-        else {
-            throw data.message;
-        }
-    })
-    .catch(function(error){
-        window.alert(error);
-    });
-}
 
 function viewBudget(){
     
@@ -446,55 +258,4 @@ function viewBudget(){
     .catch(function(error){
         window.alert(error.message);
     })
-}
-
-//create the table
-function createExpensesTable(){
-    var table = document.createElement("table");
-    table.id = 'expensesTable';
-                
-    //setup the th row
-    let trHeaders = document.createElement("tr");
-    let thNames = ['Nome', 'Categoria', 'Totale', 'Data'];
-    
-    for (i in thNames) {
-        let th = document.createElement("th");
-
-        th.innerHTML = thNames[i];
-
-        trHeaders.appendChild(th);
-    }
-
-    table.appendChild(trHeaders);
-
-    return table;
-}
-
-//fill the table
-function fillExpensesTable(userExpenses, table){
-
-    userExpenses.forEach(expense => {
-
-        let trExpense = document.createElement("tr");
-        
-        for (name in expense) {
-            if(name!="_id"){
-
-                let td = document.createElement("td");
-                
-                if(name=="date")
-                    expense[name] = clearDate(expense[name]);
-
-                td.innerHTML = expense [name];
-
-                trExpense.appendChild(td);
-            }
-
-        }
-
-        table.appendChild(trExpense);
-        
-    });
-
-    return table;
 }
