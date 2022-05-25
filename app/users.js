214dcb740eb39654a22cb2f6d03d2000e02f84b5
acolombo <alessandro.colombo-1@studenti.unitn.it>
2022-05-25 10:12:24 +0200

    Remove old route users/setBudget

--- app/users.js
+++ app/users.js
@@ -2,9 +2,6 @@ const express = require('express');
 const router = express.Router();
 const User = require('./models/user.js'); //get our user model
 
-router.get('/me', async (req, res)=>{
-    if(!req.loggedUser) return;
-});
 
 router.get('', async (req, res) => {
     let users;
@@ -60,27 +57,5 @@ function checkIfEmailInString(text) {
     return re.test(text);
 }
 
-router.post('/setBudget', async (req,res) => {
-    
-    let budget = req.body.budget; 
-    /* 
-     * take from the form the value of budget
-     * check if the budget has a correct value
-     * check if the user exists in the DB
-     * then it set the value in the DB as budget
-    */
-
-    if (!isNaN(budget) && budget > 0) {
-        let email = req.body.email;
-        let user = await User.findOne({email: email});
-        user.budget = budget;
-        await user.save();
-        res.status(200).json({success: true});
-    }
-
-    else {
-        res.status(400).json({success: false, message: "input non valido"});
-    }
-} );
 
 module.exports = router;
