a7f0bce23cfa02eaa3f5fcc48b6d5e271ea0c5db
acolombo <alessandro.colombo-1@studenti.unitn.it>
2022-05-31 17:52:20 +0200

    Add invitations route.

--- app/app.js
+++ app/app.js
@@ -18,6 +18,8 @@ const categories = require("./categories.js");
 
 const groups = require("./groups")
 
+const invitations = require("./invitations");
+
 //middleware for accessing request body
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
@@ -43,11 +45,13 @@ app.use('/api/v1/users/:id/expenses/', expenses);
 
 app.use('/api/v1/users/:id/budget', budgets);
 
-app.use('/api/v2/groups', groups)
+app.use('/api/v2/groups', groups);
+
+app.use('/api/v2/groups/:id/invitations', invitations);
 
 /*If no routs applyies, 404 error*/
 app.use((req, res) =>{
-    res.status(404).json({error: 'Not found'});
+    res.status(404).json({error: 'Page not found'});
 });
 
 
